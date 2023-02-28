7864aad347bde1bb6a8572025f168b858ea12769
Janic Holzherr <69592188+SomethingLikeAChicken@users.noreply.github.com>
2023-02-28 13:50:28 +0100

    database init

---
+++ adress-book/prisma/migrations/20230228124925_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Contacts" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "email" TEXT,
+    "description" TEXT
+);
