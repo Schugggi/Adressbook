84634f631c0d150e1a76f2e3f7bfc7ea08e19b24
Schugggi <95489210+Schugggi@users.noreply.github.com>
2023-02-28 14:01:59 +0100

    simple login

--- adress-book/src/pages/index.js
+++ adress-book/src/pages/index.js
@@ -1,15 +1,42 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+
+
+  function handleLogin(){
+
+  }
+
   return (
     <>
       <Head>
         <title>Adressbuch</title>
       </Head>
       <main className={styles.main}>
-        <h1>Adressbuch</h1>
+        <h1>Adressbuch Login</h1>
+
+        <div>
+          <p>Name</p>
+          <input value={name} onChange={(e) => setName(e.target.value)} />
+
+          <p>Nummer</p>
+          <input value={number} onChange={(e) => setNumber(e.target.value)} />
+
+          <p>Email</p>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+
+          <p>Beschreibung</p>
+          <input value={description} onChange={(e) => setDescription(e.target.value)} />
+
+          <button onClick={handleLogin}>Login</button>
+        </div>
       </main>
     </>
-  )
+  );
 }
